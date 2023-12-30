13d5a535d9df0b5dca436ad30552c8027ab29361
Anıl Çelik <anlclk11@hotmail.com>
2023-12-30 23:09:53 +0300

    sepete ekleme tamamlandı.

--- src/layouts/headerbar/headerbar.jsx
+++ src/layouts/headerbar/headerbar.jsx
@@ -1,26 +1,11 @@
 import { Link } from "react-router-dom";
 
-
 export default function HeaderBar() {
     return(
         <header className="header">
             <Link to="/">
                 <h1>VMBI</h1>
             </Link>
-            <div className="headerNav">
-                <Link to="#">
-                    <svg viewBox="0 0 24 24" >
-                        <path d="M5.651 19h12.698c-.337-1.8-1.023-3.21-1.945-4.19C15.318 13.65 13.838 13 12 13s-3.317.65-4.404 1.81c-.922.98-1.608 2.39-1.945 4.19zm.486-5.56C7.627 11.85 9.648 11 12 11s4.373.85 5.863 2.44c1.477 1.58 2.366 3.8 2.632 6.46l.11 1.1H3.395l.11-1.1c.266-2.66 1.155-4.88 2.632-6.46zM12 4c-1.105 0-2 .9-2 2s.895 2 2 2 2-.9 2-2-.895-2-2-2zM8 6c0-2.21 1.791-4 4-4s4 1.79 4 4-1.791 4-4 4-4-1.79-4-4z"></path>
-                    </svg>
-                    <h4>Hesabım</h4>
-                </Link>
-                <Link to="#">
-                    <svg fill="#fff" stroke="#000" viewBox="0 0 24 24"><circle r="1" cy="21" cx="9"></circle><circle r="1" cy="21" cx="20"></circle>
-                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
-                    </svg>
-                    <h4>Sepetim</h4>
-                </Link>
-            </div>
         </header>
     );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,10 +5,31 @@ import supabase from '~/assets/supabase/supabase';
 
 export default function Home() {
     const [products, setProducts] = useState([]);
+    const [basket, setBasket] = useState([]);
+    const [totell, setTotalPrice] = useState(0);
+
+    async function handleAddBasket(basketProducts) {
+        if(basket.some(item => item.id === basketProducts.id)) {
+            return;
+        }
+        console.log(basketProducts.id)
+        const { data: product, error } = await supabase.from('product').select('*').eq('id', basketProducts.id);
+        
+        const updateStock = product[0].stock - 1;
+
+        const { data, error: updateError } = await supabase.from('product').update({ stock: updateStock }).eq('id', product[0].id)
+        .select('id')
+        if(basket.id === basketProducts.id) {
+            return;
+        }else {
+            setBasket([...basket, basketProducts]);
+        }
+        
+    }
 
     useEffect(() => {
         async function getProducts() {
-            const { data, error } = await supabase.from('product').select()
+            const { data, error } = await supabase.from('product').select().order('created_at')
             if(error) {
                 console.log(error);
             }
@@ -17,19 +38,46 @@ export default function Home() {
         getProducts();
     }, []);
 
+    useEffect(() => {
+        totalPriceInBasket();
+    }, [basket]);
+
+    const totalPriceInBasket = () => {
+        const totalPrice = basket.reduce((acc, item) => acc + item.price, 0);
+        setTotalPrice(totalPrice)
+    }
+    
+
   return(
-    products.map(x => (
-        <div key={x.id} className='card'>
-            <img src={x.image} />
-            <div className='cardCaption'>
-                <h3>{x.name}</h3>
-                <h6>{x.description}</h6>
+    <>
+        {products.map(x => (
+            <div key={x.id} className={x.stock > 0 ? 'card' : 'softCard'}>
+                <img src={x.image} />
+                <div className='cardCaption'>
+                    <h3>{x.name}</h3>
+                    <h6>{x.description}</h6>
+                </div>
+                <div className='cardDetails'>
+                    <h4>{x.price} TL</h4>
+                    {x.stock > 0 ? ( <button className='basketBtn' onClick={() => handleAddBasket(x)}>Sepete Ekle</button> ) : ( <button disabled className='stockBtn'>Ürün tükendi</button> )
+                    }
+                </div>
             </div>
-            <div className='cardDetails'>
-                <h4>{x.price} TL</h4>
-                <button className='basketBtn'>Sepete Ekle</button>
-            </div>
-        </div>
-    ))
+        ))}
+        <details className="basketArea">
+            <summary>Sepetim</summary>
+            <ul>
+                {basket.map((x) => <li key={x.id} className='basketinProduct'>
+                    <img src={x.image} className='basketinImg' alt="" />
+                    <h4>{x.name}</h4>
+                    <p>{x.description}</p>
+                    <p>{x.price} TL</p>
+                </li>)}
+                
+            </ul>
+            <h3>Toplam Tutar: {totell} TL</h3>
+            <button className='completeOrder'>Siparişi Tamamla</button>
+        </details>
+    </>
   );
 }
